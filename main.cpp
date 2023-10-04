3845173eb0252cb1055d3b500ab0d1f8d53f0674
Orchiwi <allan.morlet13@gmail.com>
2023-10-04 17:02:31 +0200

    salut

--- main.cpp
+++ main.cpp
@@ -4,19 +4,22 @@
 #include "IRClientUDP.h"
 
 int main()
-{
+ {
     IRJournalLumineux journal;
     journal.OuvrirPortSerie("/dev/ttyUSB0");
-    journal.EnvoyerTrame(resultat);
+    journal.EnvoyerTrame("<ID00><L1><PA><FE><MA><WC><FE>BTS CN0C<E>");
     journal.FermerPortSerie();
-
+ }
+/*
     IRServeurUDP serveur;
-    OuvrirLaSocketDEcoute(4000)
-    RecevoirUnMessage(msg)
-    FermerLaSocket()
+    OuvrirLaSocketDEcoute(4000);
+    RecevoirUnMessage(msg);
+    FermerLaSocket();
     
-    IRClientUDP client
-    OuvrirLaSocketDeCommunication("172.20.21.157",40)
-    EnvoyerUnMessage(msg)
-    FermerLaSocket()
+    IRClientUDP client;
+    OuvrirLaSocketDeCommunication("172.20.21.157",40);
+    EnvoyerUnMessage(msg);
+    FermerLaSocket();
     }
+
+*/
