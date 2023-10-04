cca5d17132af0f240e5f977e1b83ea82dc589294
Orchiwi <allan.morlet13@gmail.com>
2023-10-04 16:37:44 +0200

    UDP

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "IRJournalLumineux.h"
+#include "IRServeurUDP"
 
 int main()
 {
     IRJournalLumineux journal;
     journal.OuvrirPortSerie("/dev/ttyUSB0");
-    journal.EnvoyerTrame("<ID00><L1><PA><FE><MA><WC><FE>BTS SN0C<E>");
+    journal.EnvoyerTrame(resultat);
     journal.FermerPortSerie();
+
+    IRServeurUDP serveur;
+    OuvrirLaSocketDEcoute(4000)
+    RecevoirUnMessage(msg)
+    FermerLaSocket()
 }
