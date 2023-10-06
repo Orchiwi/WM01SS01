955c28c2fe39ed19e081c16d5191ec26b196d085
Orchiwi <allan.morlet13@gmail.com>
2023-10-06 15:38:51 +0200

    Final

--- main.cpp
+++ main.cpp
@@ -2,18 +2,33 @@
 #include "IRJournalLumineux.h"
 #include "IRServeurUDP.h"
 
+using namespace std;
+
+void dechiffrement(string messageChiffre, string & message)
+{
+   string cle = "CyberSecurite";
+   for(int i = 0 ; i<messageChiffre.length() ; i++)
+   {
+      message[i] = messageChiffre[i] ^ cle [i%cle.length()];
+   }
+}
+
 
 int main()
 {  
    string msg;
    IRServeurUDP serveur;
-   serveur.OuvrirLaSocketDEcoute(4014);
+   serveur.OuvrirLaSocketDEcoute(4004);
    while(true)
 {
    serveur.RecevoirUnMessage(msg);
+
+   string trameDeChiffree = msg;
+   dechiffrement(msg, trameDeChiffree);
+
    IRJournalLumineux journal;
    journal.OuvrirPortSerie("/dev/ttyUSB0");
-   journal.EnvoyerTrame(msg);
+   journal.EnvoyerTrame(trameDeChiffree);
    journal.FermerPortSerie();
 }
    serveur.FermerLaSocket();
